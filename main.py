277b9b88f82397a4eb5f2875090de84e9ab3ce31
taraskobe <118937786+taraskobe@users.noreply.github.com>
2023-06-04 11:51:09 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+if __name__ == "__main__":
+    number = 5
+    result = factorial(number)
+    print("Factorial of", number, "is", result)
